81a8c0bc73ffaa726d7dc90373a6e8c062faba6c
Fabio <fabiomonaco007@gmail.com>
2026-02-04 09:23:23 +0100

    Fix Elf class constructor to correctly pass name parameter

--- CC6/elf.py
+++ CC6/elf.py
@@ -3,7 +3,7 @@ from classe import Character
 class Elf(Character):
     def __init__(self, name):
         super().__init__(
-            name = Elfo_Alto
+            name,
         )
         
         
