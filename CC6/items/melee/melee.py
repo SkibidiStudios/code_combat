a68461bbaecd5ef869e64197ecec614a4646a449
giulia.matarrelli <giulymata2007@gmail.com>
2026-02-04 10:11:11 +0100

    added melee weapon

--- CC6/items/melee/melee.py
+++ CC6/items/melee/melee.py
@@ -1,8 +1,8 @@
 from core.item import Item
 
 class MeleeWeapon(Item):
-    def __init__(self, name, attribute, min_damage, max_damage, two_handed):
-        super().__init__(name, "Comune")
+    def __init__(self, name, attribute, min_damage, max_damage, two_handed, rarity, description, item_type):
+        super().__init__(name, item_type, rarity, description)
         self.weapon_type = "melee damage"
         self.attribute = attribute
         self.min_damage = min_damage
@@ -10,33 +10,5 @@ class MeleeWeapon(Item):
         self.two_handed = two_handed
 
 
-class Sword(MeleeWeapon):
-    def __init__(self):
-        super().__init__("Spada", "STRENGTH", 4, 7, False)
-
-
-class BigSword(MeleeWeapon):
-    def __init__(self):
-        super().__init__("Spadone", "STRENGTH", 7, 12, True)
-
-
-class DoubleBladedAxe(MeleeWeapon):
-    def __init__(self):
-        super().__init__("Ascia Bipenne", "STRENGTH", 8, 13, True)
-
-
-class ShortSword(MeleeWeapon):
-    def __init__(self):
-        super().__init__("Spada Corta", "STRENGTH", 3, 6, False)
-
-
-class Dagger(MeleeWeapon):
-    def __init__(self):
-        super().__init__("Pugnale", "DEXTERITY", 2, 5, False)
-
-
-class DoubleBlades(MeleeWeapon):
-    def __init__(self):
-        super().__init__("Doppie Lame", "DEXTERITY", 3, 6, True)
 
 
