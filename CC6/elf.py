d7a6956e3ebd40962710b6b0232a213b3f25b92a
Fabio <fabiomonaco007@gmail.com>
2026-02-04 09:35:30 +0100

    Enhance Elf class: implement magic_attack method and refine constructor parameters for improved character functionality

--- CC6/elf.py
+++ CC6/elf.py
@@ -4,6 +4,37 @@ class Elf(Character):
     def __init__(self, name):
         super().__init__(
             name,
+            stats = {
+                'strength': 8,
+                'dexterity': 10,
+                'intelligence': 16,
+            },
+            health = 80,
+            Mana = 70,
+            defense = 0,
+            magic_defense = 0,
+            critical_chance = 1,
+            inventory = Inventory(),
+            max_health = 80,
+            max_mana = 70,
+            specie = 'Elf',
+            char_class = 'Elf',  
         )
-        
-        
+    
+    def magic_attack(self, enemy: 'Character') -> int:
+        """Calculates and returns the magic damage dealt to an enemy character."""
+        if not isinstance(enemy, Character):
+            raise TypeError("enemy must be an instance of Character")
+        is_critical = self.critical_hit()
+        if self.inventory.slots['first_hand'] is not None:
+            weapon = self.inventory.slots['first_hand']
+            weapon_damage = weapon.magic_damage
+        else:
+            weapon_damage = 1
+        if is_critical:
+            total_damage = self.modifier('intelligence') * 2 + weapon_damage
+        else:
+            total_damage = self.modifier('intelligence') + weapon_damage
+        total_damage = int(total_damage * 1.15)
+        enemy.take_damage(total_damage, 'magic')
+        return total_damage
