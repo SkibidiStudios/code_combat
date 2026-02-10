fd9d8fc1ceb05a1df6301e37d2bfbcf13055344b
DanyMazze <daniele.mazzetti08@gmail.com>
2026-02-10 09:06:25 +0100

    Rimuovi metodi e proprietà non utilizzati dalla classe Item per semplificare la gestione degli oggetti.

--- CC6/core/item.py
+++ CC6/core/item.py
@@ -17,19 +17,3 @@ class Item:
         self.item_type = item_type
         self.rarity = rarity
         self.description = description
-        self.properties = {}
-
-    def add_property(self, property_name: str):
-        """write a property to the item"""
-        self.properties[property_name] = True
-    
-    def has_property(self, property_name: str) -> bool:
-        """check if the item has a specific property"""
-        return property_name in self.properties
-
-    def get_full_name(self) -> str:
-        """Get the full name of the item, including its properties."""
-        if self.properties:
-            props = ' '.join(self.properties)
-            return f"{props} {self.name}"
-        return self.name
