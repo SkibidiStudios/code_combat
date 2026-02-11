c6dbf96c25ac9ad27f6e93e2b01f4666d4616eeb
Alice <alicenontelodico@gmail.com>
2026-02-11 08:32:43 +0100

    blah

--- warrior.py
+++ warrior.py
@@ -2,18 +2,43 @@ class Warrior:
     def __init__(self):
         self.colpo_combo = {
             'attivabile': True,
-            #scrivere indice ricarica
+            'indice_riarica': 0,
             'ricarica': 4,
             'costo_mana': 20
         }
-        
+        self.aumento_caratteristiche = {
+            'strength': 4,
+            'dexterity': 2,
+        }
 
-    def uso_abilita(self) -> bool:
-        if self.colpo_combo['attivabile']:
-            pass
-        pass #TODO
+    def uso_abilita(self, player) -> bool:
+        if self.colpo_combo['attivabile'] and self.colpo_combo['indice_riarica'] == 0 and player.inventory.slots['first_hand']['weapon_type'] == 'physical':
+            return True
+        else:
+            return False
+            
 
-    def colpo_combo(self, player):
-        if hasattr(player, 'mana') and player.Mana > 0:
-            #TODO
+    def usa_colpo_combo(self, player, target):
+        total_physical_damage = 0
+        total_magical_damage = 0
+        if hasattr(player, 'mana') and player.mana > 0:
+            if self.uso_abilita(player):
+                player.mana -= self.colpo_combo['costo_mana']
+                self.colpo_combo['indice_riarica'] = self.colpo_combo['ricarica']
+
+                physical_damage, magical_damage = player.attack(target)
+                total_physical_damage += (physical_damage / 100) * 60
+                total_magical_damage += (magical_damage / 100) * 60
+
+                physical_damage, magical_damage = player.attack(target)
+                total_physical_damage += (physical_damage / 100) * 60
+                total_magical_damage += (magical_damage / 100) * 60
+
+                physical_damage, magical_damage = player.attack(target)                
+                total_physical_damage += (physical_damage / 100) * 60
+                total_magical_damage += (magical_damage / 100) * 60
+
+                return total_physical_damage, total_magical_damage
+            else:
+                return False
                     
