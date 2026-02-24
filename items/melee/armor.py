ed06637542bec490acea3117e9b335d399b98f17
alice <alicenontelodico@gmail.com>
2026-02-24 08:46:29 +0100

    messi i file per fare il merge

---
+++ items/melee/armor.py
@@ -0,0 +1,20 @@
+from core.item import Item
+
+class Armor(Item):
+
+    pass
+
+class HeavyArmor(Armor):
+    pass
+
+
+class MidArmor(Armor):
+    pass
+
+
+class LightArmor(Armor):
+    pass
+
+
+class MagicVest(Armor):
+    pass
---
+++ items/melee/shield.py
@@ -0,0 +1,12 @@
+from core.item import Item
+
+class Shield(Item):
+
+    pass
+
+class LightShield(Shield):
+    pass
+
+
+class HeavyShield(Shield):
+    pass
